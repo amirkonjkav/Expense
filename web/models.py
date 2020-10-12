25aaf8ca3beca75addf300196b688b5c6c1c158a
amir konjkav <amirkonjkav@gmail.com>
2020-10-12 12:24:09 +0330

    add income model

--- web/models.py
+++ web/models.py
@@ -7,3 +7,13 @@ class Expense(models.Model):
     date = models.DateTimeField(auto_now=True)
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.text} - {self.amount}"
+
+class Income(models.Model):
+    text = models.CharField(max_length=250)
+    date = models.DateTimeField(auto_now=True)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
