a63ff1c9a59c08eab3389030f918a4d6b3c5cbf3
amir konjkav <amirkonjkav@gmail.com>
2020-10-12 10:47:20 +0330

    change model name

--- web/models.py
+++ web/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Expanse(models.Model):
+class Expense(models.Model):
     text = models.CharField(max_length=250)
     date = models.DateTimeField(auto_now=True)
     amount = models.BigIntegerField()
