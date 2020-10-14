b7093c78ec558b27b2a7dacc5e5bd4245437db84
amir konjkav <amirkonjkav@gmail.com>
2020-10-14 11:24:49 +0330

    add /expense/ and /income/

--- web/models.py
+++ web/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return f"{self.user}-token"
+
+
 class Expense(models.Model):
     text = models.CharField(max_length=250)
     date = models.DateTimeField(auto_now=True)
@@ -11,9 +19,12 @@ class Expense(models.Model):
     def __str__(self):
         return f"{self.text} - {self.amount}"
 
+
 class Income(models.Model):
     text = models.CharField(max_length=250)
     date = models.DateTimeField(auto_now=True)
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.text} - {self.amount}"
--- web/views.py
+++ web/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Expense, Income, Token
 
-# Create your views here.
+
+@csrf_exempt
+def submit_income(request):
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'])
+    return JsonResponse({'status': 'ok'}, encoder=JSONEncoder)
+
+
+@csrf_exempt
+def submit_expense(request):
+    print("*"*50)
+    print("we are here")
+    print(request.POST)
+    print("*"*50)
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'])
+    return JsonResponse({'status': 'ok'}, encoder=JSONEncoder)
